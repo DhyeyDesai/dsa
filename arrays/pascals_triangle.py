300d5a79562cafeaeaa440a9079f84e2fd424ce1
dhyeydesai <dhyey_desai@hotmail.com>
2026-02-13 14:00:08 +0530

    pascals triangle

---
+++ arrays/pascals_triangle.py
@@ -0,0 +1,13 @@
+# 118. Pascal's Triangle
+# https://leetcode.com/problems/pascals-triangle/
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]*(i+1) for i in range(numRows)]
+        
+        for i in range(1, numRows):
+            for j in range(1, len(res[i])-1):
+                res[i][j] = res[i-1][j-1] + res[i-1][j]
+        
+        return res
+            
